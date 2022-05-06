d828cf6cda5411b7c337b96f5e8c8f9e44a04104
SebastianHeiberg <seba3673@stud.kea.dk>
2022-05-06 23:08:43 +0200

    kan genkende tre på stribe til siden.

--- src/com/company/spil.java
+++ src/com/company/spil.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Game {
+public class spil {
 
   JFrame frame;
   Button x1x1;
@@ -17,11 +17,23 @@ public class Game {
   Button x3x1;
   Button x3x2;
   Button x3x3;
-  boolean player1 = true;
+  Button[] alleKnapper;
+  boolean player1tur = true;
 
 
+  public boolean win() {
 
-  public Game() {
+    for (int i = 0; i < alleKnapper.length; i += 3) {
+      if (!alleKnapper[i].getLabel().isEmpty() && alleKnapper[i].getLabel().equals(alleKnapper[i + 1].getLabel()) && alleKnapper[i].getLabel().equals(alleKnapper[i + 2].getLabel())) {
+        System.out.println("virker!");
+        return true;
+      }
+    }
+
+    return true;
+  }
+
+  public spil() {
     frame = new JFrame("Kryds og bolle");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -46,94 +58,96 @@ public class Game {
     frame.add(x3x2);
     x3x3 = new Button();
     frame.add(x3x3);
-    frame.setPreferredSize(new Dimension(400,400));
+    frame.setPreferredSize(new Dimension(400, 400));
     frame.pack();
     frame.setLocationRelativeTo(null);
   }
 
   public static void main(String[] args) {
     // write your code here
-    Game game = new Game();
+    spil game = new spil();
     game.run();
   }
 
-  public void switchTurn (){
-    this.player1 = !player1;
+  public void setTur() {
+    this.player1tur = !player1tur;
   }
 
-  public void setBox (Button button){
+  public void knapAction(Button button) {
     if (button.getLabel().isEmpty()) {
-      if (player1) {
+      if (player1tur) {
         button.setLabel("X");
       } else {
         button.setLabel("0");
       }
-      switchTurn();
+      setTur();
+      win();
     }
   }
 
-  public void knapper () {
+  public void tilføjActionTilKnapper() {
     //første række
     x1x1.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        setBox(x1x1);
+        knapAction(x1x1);
       }
     });
     x1x2.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        setBox(x1x2);
+        knapAction(x1x2);
       }
     });
     x1x3.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        setBox(x1x3);
+        knapAction(x1x3);
       }
     });
     //anden række
     x2x1.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        setBox(x2x1);
+        knapAction(x2x1);
       }
     });
     x2x2.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        setBox(x2x2);
+        knapAction(x2x2);
       }
     });
     x2x3.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        setBox(x2x3);
+        knapAction(x2x3);
       }
     });
     // tredje række
     x3x1.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        setBox(x3x1);
+        knapAction(x3x1);
       }
     });
     x3x2.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        setBox(x3x2);
+        knapAction(x3x2);
       }
     });
     x3x3.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        setBox(x3x3);
+        knapAction(x3x3);
       }
     });
   }
 
   private void run() {
-  knapper();
+    alleKnapper = new Button[]{x1x1, x1x2, x1x3, x2x1, x2x2, x2x3, x3x1, x3x2, x3x3};
+    tilføjActionTilKnapper();
 
 
   }
